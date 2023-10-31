cd0fd057ff4804326d943eae6ef7b895300b2cb8
Khauan Bortuluci <khauanbs@gmail.com>
2023-10-31 11:32:24 -0300

    Receive this magnificent calculator.

---
+++ basic-calculator.py
@@ -0,0 +1,30 @@
+#This is a basic calculator. I'm bored, haha!
+online = 1
+ 
+while online == 1:
+  number1 = float(input("insert the first number: "))
+  operator = input("insert the operator: ") # + | - | * | /
+  number2 = float(input("Insert the second number: "))
+    
+  operator = operator
+  if operator == "+":
+    result = number1+number2
+    print("The result is ", result)
+  elif operator == "-":
+    result = number1-number2
+    print("The result is ", result)
+  elif operator == "*":
+    result = number1*number2
+    print("The result is ", result)
+  elif operator == "/":
+    if number2 != 0:
+      result = number1/number2
+      print("The result is ", result)
+    else:
+      print("Error! Division by zero.")
+  else:
+    print("Error! Invalid values entered.")
+    
+  online = int(input("Do you need another calculation? [1]Yes / [0]No: "))
+  if online == 0:
+    break
